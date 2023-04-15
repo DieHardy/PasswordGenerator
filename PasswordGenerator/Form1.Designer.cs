75d04cc490e6aa1726f5ace77948d07171ec8773
DiHardy <nerdyaus21@gmail.com>
2023-04-15 19:11:57 +0300

    PG Version 1.0
    Password generator made in c#. Version 1.0

---
+++ PasswordGenerator/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿
+namespace PasswordGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 23.25F);
+            this.label1.Location = new System.Drawing.Point(40, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(329, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Password Generator";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.SeaShell;
+            this.textBox1.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.textBox1.Location = new System.Drawing.Point(233, 75);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(115, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.25F);
+            this.label2.Location = new System.Drawing.Point(25, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(207, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Length of a password:";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.25F);
+            this.checkBox1.Location = new System.Drawing.Point(29, 127);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(128, 25);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Lowercase";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.25F);
+            this.checkBox2.Location = new System.Drawing.Point(29, 170);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(127, 25);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "Uppercase";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.25F);
+            this.checkBox3.Location = new System.Drawing.Point(29, 215);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(110, 25);
+            this.checkBox3.TabIndex = 5;
+            this.checkBox3.Text = "Numbers";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.25F);
+            this.checkBox4.Location = new System.Drawing.Point(29, 261);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(104, 25);
+            this.checkBox4.TabIndex = 6;
+            this.checkBox4.Text = "Symbols";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.OrangeRed;
+            this.button1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.25F);
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(226, 153);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 61);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 23.25F);
+            this.label3.Location = new System.Drawing.Point(152, 299);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 36);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Result";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.25F);
+            this.button2.Location = new System.Drawing.Point(152, 406);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 32);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Copy";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.25F);
+            this.textBox2.Location = new System.Drawing.Point(20, 338);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(375, 62);
+            this.textBox2.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Password Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ PasswordGenerator/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PasswordGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string numbers = "0123456789";
+        string lowercase = "abcdefghijklmnopqrstuvwxyz";
+        string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        string symbols = @"`~!@#$%^&*()–_=+[{}]\|;:‘“,./<>?";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string parameters = "";
+            int counterParameters = 0;
+            int lengthPassword = 0;
+            try
+            {
+                lengthPassword = Int32.Parse(textBox1.Text);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("You need to type a number!");
+                textBox1.Text = "";
+            }
+            if (checkBox1.Checked == true)
+            {
+                parameters += lowercase;
+                counterParameters += lowercase.Length;
+            }
+            if (checkBox2.Checked == true)
+            {
+                parameters += uppercase;
+                counterParameters += uppercase.Length;
+            }
+            if (checkBox3.Checked == true)
+            {
+                parameters += numbers;
+                counterParameters += numbers.Length;
+            }
+            if (checkBox4.Checked == true)
+            {
+                parameters += symbols;
+                counterParameters += symbols.Length;
+            }
+            Random rnd = new Random();
+            if (lengthPassword == 0)
+            {
+                MessageBox.Show("You can`t create a password with 0 symbols!");
+                textBox2.Text = "";
+            }
+            else if (checkBox1.Checked == false && checkBox2.Checked == false && checkBox3.Checked == false && checkBox4.Checked == false)
+            {
+                MessageBox.Show("Choose at least one of the parameter!");
+                textBox2.Text = "";
+            }
+            else
+            {
+                textBox2.Text = "";
+
+                for (int i = 0; i < lengthPassword; i++)
+                {
+                    textBox2.Text += parameters[rnd.Next(0, counterParameters)]; ;
+                }
+            }
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetData(DataFormats.Text, (Object)textBox2.Text);
+        }
+    }
+}
